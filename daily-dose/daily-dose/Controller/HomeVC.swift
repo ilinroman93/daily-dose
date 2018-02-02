49d7d6ac76b0d99b0f7891ad16360c8778bd624c
Roman Ilin <ilrose887@gmail.com>
2018-02-02 23:42:29 +0800

    user can make purchases

--- daily-dose/daily-dose/Controller/HomeVC.swift
+++ daily-dose/daily-dose/Controller/HomeVC.swift
@@ -13,19 +13,36 @@ class HomeVC: UIViewController {
     
     @IBOutlet weak var bannerView: GADBannerView!
     
-
+    @IBOutlet weak var removeAdsBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
-        bannerView.rootViewController = self
-        bannerView.load(GADRequest())
+        if UserDefaults.standard.bool(forKey: PurchaseManager.instance.IAP_REMOVE_ADS) {
+            removeAdsBtn.removeFromSuperview()
+            bannerView.removeFromSuperview()
+        } else {
+            bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+            bannerView.rootViewController = self
+            bannerView.load(GADRequest())
+        }
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func removeAdsPressed(_ sender: Any) {
+        // show loading spinner Activity indicator
+        PurchaseManager.instance.purchaseRemoveAds { (success) in
+            // dismiss the spinner
+            if succes {
+                self.bannerView.removeFromSuperview()
+                self.removeAdsBtn.removeFromSuperview()
+            } else {
+                // show message to the user
+            }
+        }
     }
+    
 
 
 }
--- daily-dose/daily-dose/Services/PurchaseManager.swift
+++ daily-dose/daily-dose/Services/PurchaseManager.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2018 Romax. All rights reserved.
 //
 
+typealias CompletionHandler = (_ success: Bool) -> ()
+
 import Foundation
 import StoreKit
 
@@ -17,12 +19,25 @@ class PurchaseManager: NSObject, SKProductsRequestDelegate, SKPaymentTransaction
     
     var productsRequest: SKProductsRequest!
     var products = [SKProduct]()
+    var transactionComplete: CompletionHandler?
     
     func fetchProducts() {
         let productsIds = NSSet(object: IAP_REMOVE_ADS) as! Set<String>
         productsRequest = SKProductsRequest(productIdentifiers: productsIds)
         productsRequest.delegate = self
         productsRequest.start()
+    }
+    
+    func purchaseRemoveAds(onComplete: @escaping CompletionHandler) {
+        if SKPaymentQueue.canMakePayments() && products.count > 0 {
+            transactionComplete = onComplete
+            let removeAdsProduct = products[0]
+            let payment = SKPayment(product: removeAdsProduct)
+            SKPaymentQueue.default().add(self)
+            SKPaymentQueue.default().add(payment)
+        } else {
+            onComplete(false)
+        }
     }
     
     func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
@@ -32,7 +47,28 @@ class PurchaseManager: NSObject, SKProductsRequestDelegate, SKPaymentTransaction
     }
     
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-        
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .purchased:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                if transaction.payment.productIdentifier == IAP_REMOVE_ADS {
+                    UserDefaults.standard.set(true, forKey: IAP_REMOVE_ADS)
+                    transactionComplete?(true)
+                }
+                break
+            case .failed:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                transactionComplete?(false)
+                break
+            case .restored:
+                SKPaymentQueue.default().finishTransaction(transaction)
+                transactionComplete?(true)
+                break
+            default:
+                transactionComplete?(false)
+                break
+            }
+        }
     }
     
     
