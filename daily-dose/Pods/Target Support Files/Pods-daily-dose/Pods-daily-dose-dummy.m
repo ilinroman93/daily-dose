e2a6d4723e7e7409948b2fa62b15db645610bf4e
Roman Ilin <ilrose887@gmail.com>
2018-02-02 17:08:11 +0800

    320x50 google mobile ad added

---
+++ daily-dose/Pods/Target Support Files/Pods-daily-dose/Pods-daily-dose-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_daily_dose : NSObject
+@end
+@implementation PodsDummy_Pods_daily_dose
+@end
---
+++ daily-dose/daily-dose/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  daily-dose
+//
+//  Created by Ильин Роман on 02.02.2018.
+//  Copyright © 2018 Romax. All rights reserved.
+//
+
+import UIKit
+import GoogleMobileAds
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var bannerView: GADBannerView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
